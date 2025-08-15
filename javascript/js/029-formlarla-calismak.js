18842b1f2b6f6b1977d9ae021771234f51bb0371
busraygul <aygul.busra58@gmail.com>
2025-08-15 15:37:20 +0300

    form-sonu-ecx

--- javascript/js/029-formlarla-calismak.js
+++ javascript/js/029-formlarla-calismak.js
@@ -3,7 +3,15 @@ let userFormDOM = document.querySelector("#userForm")
 userFormDOM.addEventListener('submit', formHandler)
 const alertDOM = document.querySelector("#alert")
 
-const alertFunction = (title, message, className= "warning") =>`
+// let clearDOM = document.querySelector("#clear")
+
+// clearDOM.addEventListener('click',clear())
+
+// function clear (){
+//     userFormDOM.innerHTML = "" 
+// }
+
+const alertFunction = (title, message, className = "warning") => `
 <div class="alert alert-${className} alert-dismissible fade show" role="alert">
   <strong>${title}</strong> ${message}
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
@@ -17,18 +25,29 @@ function formHandler(event) {
     const SCORE = document.querySelector("#score")
 
     if (USER_NAME.value && SCORE.value) {
+        //bilgiler doğruysa uyarıyı kapatıyor.
+        const alertBox = document.querySelector('.alert');
+        if (alertBox) {
+            alertBox.style.display = 'none';
+        }
+
+
         addItem(USER_NAME.value, SCORE.value)
+
+        if (!document.querySelector("#clearListBtn")) {
+            addClearButton();
+        }
+
         USER_NAME.value = ""//Gönderdikten sonra sıfırladık.
         SCORE.value = ""
-    }else{
+    } else {
         alertDOM.innerHTML = alertFunction(
             "Başlık Bilgisi",
             "Eksik Bilgi Girdiniz!",
-        "danger")
+            "danger")
     }
-
-
 }
+
 let userListDOM = document.querySelector("#userList")
 
 const addItem = (userName, score) => {
@@ -39,4 +58,16 @@ const addItem = (userName, score) => {
     liDOM.classList.add(
         'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
     userListDOM.append(liDOM)
+}
+function addClearButton(){
+    const btn = document.createElement("button")
+    btn.id = "clearListBtn"
+    btn.className = "btn btn-danger mt-3"
+    btn.textContent = "Listeyi Temizle"
+
+    btn.addEventListener("click", function(){
+        userListDOM.innerHTML = ""
+        btn.remove()
+    })
+    userListDOM.parentElement.appendChild(btn);
 }
